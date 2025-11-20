3f623130c96be98aa232a5e9d36bb0322c170674
Reinaldo Contreras <reinaldojcg1@gmail.com>
2025-11-20 11:49:10 -0500

    Add findAll endpoint with pagination

--- src/pokemons/pokemons.service.ts
+++ src/pokemons/pokemons.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { PaginationDto } from '../shared/dto/pagination.dto';
 
 @Injectable()
 export class PokemonsService {
@@ -8,8 +9,17 @@ export class PokemonsService {
     return 'This action adds a new pokemon';
   }
 
-  findAll() {
-    return `This action returns all pokemons`;
+  async findAll(paginationDto: PaginationDto) {
+    const { page = 1, limit = 10 } = paginationDto;
+    const offset = (page - 1) * limit;
+
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+
+    const res = await fetch(url);
+
+    const data = await res.json();
+
+    return data;
   }
 
   findOne(id: number) {
